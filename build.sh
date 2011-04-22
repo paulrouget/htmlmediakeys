27d44af01d865bb7e2dc5f64c658b73bce5ac023
Paul Rouget <paul.rouget@gmail.com>
2011-04-22 20:27:46 +0200

    Don't need this script anymore

--- build.sh
+++
@@ -1,2 +0,0 @@
-gcc -g -O2 -pipe -Wall -pthread  `pkg-config dbus-glib-1 --cflags` -c -o mediakeys.o mediakeys.c
-gcc -o mediakeys.so mediakeys.o -pthread -shared `pkg-config dbus-glib-1 --libs` -fPIC
